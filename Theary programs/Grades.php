2df95c595fe2154a1a7d71c7d3299e82a11c8bbd
Suyog Chavan <suyogschavan03@gmail.com>
2022-03-02 20:37:40 +0530

    ongoing

---
+++ Theary programs/Grades.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Program to find grades of 3 subjects by using if else ladder</title>
+</head>
+<body>
+    <form method="post">
+        Enter the marks of Maths : <input type="number" name="M_m"><br><br>
+        Enter the marks of Science : <input type="number" name="M_s"><br><br>
+        Enter the marks of Language : <input type="number" name="M_l"><br><br>
+
+        <input type="submit" name="submit">
+        
+    </form>
+    <?php 
+    
+        $Maths = $_POST['M_m'];
+        $Sci = $_POST['M_s'];
+        $Lang = $_POST['M_l'];
+
+        if
+
+    ?>
+</body>
+</html>
