51ae598701f4d4937153325a7607888dbda75555
Suyog Chavan <suyogschavan03@gmail.com>
2022-05-16 23:48:58 +0530

    MC

--- EX15/connection.php
+++ EX15/connection.php
@@ -8,9 +8,4 @@ $dbname = "ex15";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// if ($conn) {
-//     echo "Connected to database";
-// }else {
-//     echo "not connected";
-// }
 ?>
